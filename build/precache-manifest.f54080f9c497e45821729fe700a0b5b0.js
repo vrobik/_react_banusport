859d437ca9cb46b2b4d2f09399aec520859b6b2e
vrobik <robuvitalik@gmail.com>
2021-07-14 19:38:34 +0300

    2

---
+++ build/precache-manifest.f54080f9c497e45821729fe700a0b5b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f53a09cbf16132597854a90becd285",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0937633a1d0d26a4aba",
+    "url": "./static/css/4.3a3c4c6f.chunk.css"
+  },
+  {
+    "revision": "63cce624334bb8486fe0",
+    "url": "./static/css/main.1b12d644.chunk.css"
+  },
+  {
+    "revision": "a8df71cb20958aece429",
+    "url": "./static/js/2.b0b887d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b0b887d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a029b287ef8d7f8e4ed1",
+    "url": "./static/js/3.47907fba.chunk.js"
+  },
+  {
+    "revision": "882bc10f74612fbae0e25332a0a17f73",
+    "url": "./static/js/3.47907fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0937633a1d0d26a4aba",
+    "url": "./static/js/4.a7ca74b5.chunk.js"
+  },
+  {
+    "revision": "6a158f341dc1e3049568872cfa992e48",
+    "url": "./static/js/4.a7ca74b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614cd881e71b83cdbebb",
+    "url": "./static/js/5.facd61f7.chunk.js"
+  },
+  {
+    "revision": "b2c553861650b87a2a3d",
+    "url": "./static/js/6.07176aae.chunk.js"
+  },
+  {
+    "revision": "63cce624334bb8486fe0",
+    "url": "./static/js/main.76fac11c.chunk.js"
+  },
+  {
+    "revision": "57b947ebfe20ebbdf3ae",
+    "url": "./static/js/runtime-main.da971a5c.js"
+  }
+]);
