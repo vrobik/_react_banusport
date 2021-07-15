74674b55abe24ea424ac66ad7ece1c0bc685a3da
vrobik <robuvitalik@gmail.com>
2021-07-15 18:08:02 +0300

    Updates

---
+++ precache-manifest.a23aae000a4e43a6e1d2c01127250418.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52afc7c7c1bb7c8a28513b0a03fa5edb",
+    "url": "/_react_banusport/index.html"
+  },
+  {
+    "revision": "aceeeb72ae43f1b93db0",
+    "url": "/_react_banusport/static/css/4.91b9a3e6.chunk.css"
+  },
+  {
+    "revision": "f4c8a1087ffce925baad",
+    "url": "/_react_banusport/static/css/main.94266e43.chunk.css"
+  },
+  {
+    "revision": "bf9d455b025236491b5f",
+    "url": "/_react_banusport/static/js/2.21fa8ac1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/_react_banusport/static/js/2.21fa8ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a833872084b8486052a8",
+    "url": "/_react_banusport/static/js/3.dbfedca0.chunk.js"
+  },
+  {
+    "revision": "882bc10f74612fbae0e25332a0a17f73",
+    "url": "/_react_banusport/static/js/3.dbfedca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aceeeb72ae43f1b93db0",
+    "url": "/_react_banusport/static/js/4.09b7f243.chunk.js"
+  },
+  {
+    "revision": "6a158f341dc1e3049568872cfa992e48",
+    "url": "/_react_banusport/static/js/4.09b7f243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba7bbf32386c8432485",
+    "url": "/_react_banusport/static/js/5.f02c4ca2.chunk.js"
+  },
+  {
+    "revision": "f7033b6d8abc021b6040",
+    "url": "/_react_banusport/static/js/6.7566bd7a.chunk.js"
+  },
+  {
+    "revision": "f4c8a1087ffce925baad",
+    "url": "/_react_banusport/static/js/main.a1ef40ad.chunk.js"
+  },
+  {
+    "revision": "9b92c1f5f218ae106178",
+    "url": "/_react_banusport/static/js/runtime-main.ae8632a3.js"
+  }
+]);
