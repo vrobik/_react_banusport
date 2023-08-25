8283491aac64e4fa3ed4c92eb795b398008f83c8
vrobik <robuvitalik@gmail.com>
2023-08-25 22:31:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/_react_banusport/precache-manifest.36988c7c3dff5a9c3da2f708e161299f.js"
+  "/_react_banusport/precache-manifest.f36f58aed53293cd97194ea3ec884490.js"
 );
 
 self.addEventListener('message', (event) => {
